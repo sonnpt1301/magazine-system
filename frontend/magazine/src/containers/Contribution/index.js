94fe806695da9e4c25391af2ed577a37fafb819e
sonnpt1301 <sonnpt1301@gmail.com>
2021-02-22 23:47:13 +0700

    manager, guest view contribution

--- frontend/magazine/src/containers/Contribution/index.js
+++ frontend/magazine/src/containers/Contribution/index.js
@@ -18,8 +18,7 @@ const Contribution = () => {
     const [comment, setComment] = useState(contribution.comments)
     const [contributionId, setContributionId] = useState('')
     const [content, setContent] = useState('')
-
-
+    const [checkbox, setCheckbox] = useState(false)
     const handleUploadFile = (e) => {
         let file = e.target.files
         for (let i = 0; i < file.length; i++) {
@@ -29,6 +28,7 @@ const Contribution = () => {
             )
         }
     }
+
     const handleUpdateUploadFile = (e) => {
         let file = e.target.files
         for (let i = 0; i < file.length; i++) {
@@ -39,8 +39,7 @@ const Contribution = () => {
         }
     }
 
-
-    const uploadFile = () => {
+    const _uploadFile = () => {
         const form = new FormData()
         form.append('title', title)
         form.append('description', description)
@@ -53,7 +52,6 @@ const Contribution = () => {
     const _updateContribution = (id) => {
         const form = new FormData()
         const params = { id }
-        console.log(params)
         form.append('title', title)
         form.append('description', description)
         for (let file of updateFileUpload) {
@@ -74,13 +72,11 @@ const Contribution = () => {
     const _publishContribution = () => {
         const body = { ids: [contributionId] }
         dispatch(publishContribution(body))
-        console.log(body)
     }
 
     const _downloadFile = (fileName, id) => {
         const file = fileUpload.find(x => x._id === id)
         const fileId = file._id
-        console.log(fileId)
         const params = { fileName, contributionId, fileId }
         dispatch(downloadFile(params))
     }
@@ -88,20 +84,19 @@ const Contribution = () => {
     const _addComment = (id) => {
         const params = { contributionId: id }
         const body = { content }
-        dispatch(addComment(params, body))
-
+        dispatch(addComment(params, body)).then(() => setContent(''))
     }
 
+    const handleCheckbox = () => {
+        setCheckbox(!checkbox)
+    }
 
     useEffect(() => {
         setAllContribution(contribution.allContributions)
         setComment(contribution.comments)
     }, [contribution.allContributions, contribution.comments])
 
-
-
     return (
-
         <Layout>
             {/* -------------------------------------------------------------------------------------------------------------------------------------------------------- */}
             {/* Start Student Display */}
@@ -118,7 +113,7 @@ const Contribution = () => {
                                                 <h4 className="form-header text-uppercase">
                                                     <i className="fa fa-user-circle-o"></i>
                                                 Upload Contribution
-                                        </h4>
+                                                </h4>
                                                 <div class="form-group row">
                                                     <label for="input-1" class="col-sm-2 col-form-label">Title</label>
                                                     <div class="col-sm-10">
@@ -137,10 +132,12 @@ const Contribution = () => {
                                                         <input type="file" className="form-control" name={fileUpload} multiple="multiple" onChange={handleUploadFile} required />
                                                     </div>
                                                 </div>
-
+                                                <div class="icheck-material-white">
+                                                    <input type="checkbox" id="terms_and_conditions" onClick={handleCheckbox} />
+                                                    <label for="terms_and_conditions">I have read and agree to Terms and Conditions</label>
+                                                </div>
                                                 <div className="form-footer">
-                                                    <button type="submit" className="btn btn-danger"><i className="fa fa-times"></i> CANCEL</button>
-                                                    <button type="submit" className="btn btn-success" onClick={uploadFile}><i className="fa fa-check-square-o"></i> Upload</button>
+                                                    <button type="submit" id="btn-upload" disabled={checkbox ? false : true} className="btn btn-success" onClick={_uploadFile}><i className="fa fa-check-square-o"></i> Upload</button>
                                                 </div>
                                             </form>
                                         </div>
@@ -196,7 +193,6 @@ const Contribution = () => {
                                                     </ul>
                                                 )).reverse()
                                             }
-
                                         </div>
                                     </div>
 
@@ -231,7 +227,6 @@ const Contribution = () => {
                                                 </div>
                                             ))
                                         }
-
                                         <div className="form-group">
                                             <button type="submit" className="btn btn-light px-5" onClick={() => { _updateContribution(contributionId) }} data-dismiss="modal" aria-label="Close" aria-hidden="true"><i className="icon-lock"></i> Update</button>
                                         </div>
@@ -239,7 +234,6 @@ const Contribution = () => {
 
                                 </div>
                             </div>
-
                         </div>
                     </div>
                 )
@@ -248,7 +242,7 @@ const Contribution = () => {
             {/* End Student Display */}
             {/* -------------------------------------------------------------------------------------------------------------------------------------------------------- */}
 
-
+            {/* -------------------------------------------------------------------------------------------------------------------------------------------------------- */}
 
             {/* -------------------------------------------------------------------------------------------------------------------------------------------------------- */}
             {/* Start Coordinator Display */}
@@ -300,21 +294,17 @@ const Contribution = () => {
                                                             }
                                                             <input type="text" class="form-control form-control-rounded mt-3"
                                                                 placeholder="Write comment here..."
-                                                                defaultValue={content}
                                                                 onChange={(e) => setContent(e.target.value)}
                                                                 onKeyPress={event => event.key === 'Enter' && _addComment(x._id)}
                                                             >
                                                             </input>
-
                                                         </div>
                                                     </li>
                                                 </ul>
                                             )).reverse()
                                         }
-
                                     </div>
                                 </div>
-
                                 <Modal
                                     id={'detailModal'}
                                     modaltitle={'Contribution Detail'}
@@ -323,8 +313,6 @@ const Contribution = () => {
                                     <h4 className="mb-0">{title}</h4>
                                     <br />
                                     <p>{description}</p>
-
-
                                     {
                                         fileUpload.map((file) => (
                                             <div class="card">
@@ -334,32 +322,161 @@ const Contribution = () => {
                                             </div>
                                         ))
                                     }
-
                                     <div className="form-group">
                                         <button type="submit" className="btn btn-light px-5" onClick={_publishContribution} data-dismiss="modal" aria-label="Close" aria-hidden="true"><i className="icon-lock"></i> Publish</button>
                                     </div>
                                 </Modal>
-
                             </div>
                         </div>
                     </div>
                 </div>
-
             )
             }
             {/* -------------------------------------------------------------------------------------------------------------------------------------------------------- */}
             {/* End Coordinator Display */}
             {/* -------------------------------------------------------------------------------------------------------------------------------------------------------- */}
 
-
-
+            {/* -------------------------------------------------------------------------------------------------------------------------------------------------------- */}
 
             {/* -------------------------------------------------------------------------------------------------------------------------------------------------------- */}
             {/* Start Manager Display */}
             {/* -------------------------------------------------------------------------------------------------------------------------------------------------------- */}
 
+            {
+                user.role === 'manager' && (
+                    <div className="content-wrapper">
+                        <div className="container-fluid">
+                            <div className="row">
+
+                                {
+                                    allContribution.filter(contr => contr.is_public === true).map(contr => (
+                                        <div class="col-lg-4">
+                                            <div class="card">
+                                                <div class="card-header text-uppercase">Contributions</div>
+                                                <div class="card-body">
+                                                    <ul class="list-unstyled">
+                                                        <li class="media">
+                                                            <img class="mr-3 rounded" src="https://via.placeholder.com/110x110" alt="user avatar" />
+                                                            <div class="media-body">
+                                                                <h5 class="mt-0 mb-1">{contr.user_info.lastName}
+                                                                    <span>
+                                                                        <button data-toggle="modal" data-target="#detailModal" className="btn btn-light btn-sm waves-effect waves-light m-1 pull-right"
+                                                                            onClick={(e) => { handleShowDetailModal(contr._id) }}><i className="fa fa-edit"></i></button>
+                                                                    </span>
+                                                                </h5>
+                                                                <p>{contr.title}</p>
+                                                                {
+                                                                    contr.is_public ?
+                                                                        <span className="badge badge-success shadow-success m-1">Published</span> :
+                                                                        <span className="badge badge-light shadow-light m-1">Pending</span>
+                                                                }
+                                                            </div>
+                                                        </li>
+                                                    </ul>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    ))
+                                }
+
+                            </div>
+                        </div>
+                        <Modal
+                            id={'detailModal'}
+                            modaltitle={'Contribution Detail'}
+                        >
+                            <h4 className="mb-0">{author}</h4>
+                            <h4 className="mb-0">{title}</h4>
+                            <br />
+                            <p>{description}</p>
+                            {
+                                fileUpload.map((file) => (
+                                    <div class="card">
+                                        <div class="card-body">
+                                            <div style={{ cursor: 'pointer' }} onClick={() => { _downloadFile(file.fileName, file._id) }}>{file.fileName} <i aria-hidden="true" class="fa fa-download pull-right"></i> </div>
+                                        </div>
+                                    </div>
+                                ))
+                            }
+                        </Modal>
+                    </div>
+
+                )
+            }
+
             {/* -------------------------------------------------------------------------------------------------------------------------------------------------------- */}
             {/* End Manager Display */}
+            {/* -------------------------------------------------------------------------------------------------------------------------------------------------------- */}
+
+
+            {/* -------------------------------------------------------------------------------------------------------------------------------------------------------- */}
+            {/* Start Guest Display */}
+            {/* -------------------------------------------------------------------------------------------------------------------------------------------------------- */}
+
+            {
+                user.role === 'guest' && (
+                    <div className="content-wrapper">
+                        <div className="container-fluid">
+                            <div className="row">
+
+                                {
+                                    allContribution.filter(contr => contr.is_public === true && contr.facultyId === user.facultyId).map(contr => (
+                                        <div class="col-lg-4">
+                                            <div class="card">
+                                                <div class="card-header text-uppercase">Contributions</div>
+                                                <div class="card-body">
+                                                    <ul class="list-unstyled">
+                                                        <li class="media">
+                                                            <img class="mr-3 rounded" src="https://via.placeholder.com/110x110" alt="user avatar" />
+                                                            <div class="media-body">
+                                                                <h5 class="mt-0 mb-1">{contr.user_info.lastName}
+                                                                    <span>
+                                                                        <button data-toggle="modal" data-target="#detailModal" className="btn btn-light btn-sm waves-effect waves-light m-1 pull-right"
+                                                                            onClick={(e) => { handleShowDetailModal(contr._id) }}><i className="fa fa-edit"></i></button>
+                                                                    </span>
+                                                                </h5>
+                                                                <p>{contr.title}</p>
+                                                                {
+                                                                    contr.is_public ?
+                                                                        <span className="badge badge-success shadow-success m-1">Published</span> :
+                                                                        <span className="badge badge-light shadow-light m-1">Pending</span>
+                                                                }
+                                                            </div>
+                                                        </li>
+                                                    </ul>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    ))
+                                }
+
+                            </div>
+                        </div>
+                        <Modal
+                            id={'detailModal'}
+                            modaltitle={'Contribution Detail'}
+                        >
+                            <h4 className="mb-0">{author}</h4>
+                            <h4 className="mb-0">{title}</h4>
+                            <br />
+                            <p>{description}</p>
+                            {
+                                fileUpload.map((file) => (
+                                    <div class="card">
+                                        <div class="card-body">
+                                            <div style={{ cursor: 'pointer' }} onClick={() => { _downloadFile(file.fileName, file._id) }}>{file.fileName} <i aria-hidden="true" class="fa fa-download pull-right"></i> </div>
+                                        </div>
+                                    </div>
+                                ))
+                            }
+                        </Modal>
+                    </div>
+
+                )
+            }
+
+            {/* -------------------------------------------------------------------------------------------------------------------------------------------------------- */}
+            {/* End Guest Display */}
             {/* -------------------------------------------------------------------------------------------------------------------------------------------------------- */}
 
         </Layout >
